c971ceadbba16609bf3856a1a2b006b3b0afc4d1
Vladimir Gulev <vladinson009@icloud.com>
2026-05-07 21:08:52 +0200

    updated package.json

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
     // ... Specify options here.
   },
 });
