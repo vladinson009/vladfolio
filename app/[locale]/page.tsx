7dd4a6276ec916e134a7e85994b20cb21e90e4b7
Vladimir Gulev <vladinson009@icloud.com>
2026-05-09 18:07:08 +0200

    Add: new section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,14 +1,22 @@
 import PageSeparator from '@/components/shared/page-separator';
 import { IntroduceSection } from './sections/introduce/introduce-section';
 import ProjectsSection from './sections/projects/projects-section';
+import { useTranslations } from 'next-intl';
+import CertificatesSection from './sections/certificates/certificates-section';
 
 export default function HomePage() {
+  const t = useTranslations('PageSeparator');
+  const projects = t('projects');
+  const certificates = t('certificates');
+
   return (
     <>
       <IntroduceSection />
-      <PageSeparator title="Projects" href="/projects" />
+      <PageSeparator title={projects} href="/projects" />
       <ProjectsSection />
-      <PageSeparator title="Certificates" href="/certificates" />
+      <PageSeparator title={certificates} href="/certificates" />
+      <CertificatesSection />
+      <PageSeparator title="Unknown" href="/certificates" />
     </>
   );
 }
